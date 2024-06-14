3dbad58b0d74d645781c289468ec039f360a96ca
Malik <timurkaev_malik@icloud.com>
2024-06-14 21:04:05 +0300

    добавил userPhoto и его конфигурацию

--- FakeNFT/Scenes /ProfileVeiwController/ProfileViewController.swift
+++ FakeNFT/Scenes /ProfileVeiwController/ProfileViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    let servicesAssembly: ServicesAssembly
-
+    private lazy var userPhoto = UIImageView(image: UIImage(systemName: "person.crop.circle"))
+    
+    private let servicesAssembly: ServicesAssembly
+    
     init(servicesAssembly: ServicesAssembly) {
         self.servicesAssembly = servicesAssembly
         super.init(nibName: nil, bundle: nil)
@@ -23,5 +25,23 @@ final class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "YPWhite")
+        configureUserPhoto()
+    }
+    
+    private func configureUserPhoto() {
+        
+        userPhoto.tintColor = UIColor(named: "YPBlack")
+        userPhoto.layer.masksToBounds = true
+        userPhoto.layer.cornerRadius = 25
+        
+        userPhoto.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userPhoto)
+        
+        NSLayoutConstraint.activate([
+            userPhoto.widthAnchor.constraint(equalToConstant: 70),
+            userPhoto.heightAnchor.constraint(equalToConstant: 70),
+            userPhoto.topAnchor.constraint(equalTo: view.topAnchor, constant: 108),
+            userPhoto.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
     }
 }
