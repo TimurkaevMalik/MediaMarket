ef4e664695db6290b7dfa463333879790db08737
a.krasnov <wonsarc@icloud.com>
2024-06-21 19:06:55 +0300

    add navBarItem. done  task [M1][Верстка Сортировки] Добавление кнопки сортировки в VC #89

--- FakeNFT/Scenes /CatalogViewController/CatalogViewController.swift
+++ FakeNFT/Scenes /CatalogViewController/CatalogViewController.swift
@@ -2,7 +2,13 @@ import UIKit
 
 final class CatalogViewController: UIViewController {
 
+    // MARK: - Public Properties
+
     let servicesAssembly: ServicesAssembly
+
+    // MARK: - Private Properties
+
+    // MARK: - Initializers
 
     init(servicesAssembly: ServicesAssembly) {
         self.servicesAssembly = servicesAssembly
@@ -13,8 +19,26 @@ final class CatalogViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Overrides Methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(named: "YPWhite")
+        view.backgroundColor = .systemBackground
+        setupNavBar()
+    }
+
+    // MARK: - Public Methods
+
+    // MARK: - Private Methods
+
+    private func setupNavBar() {
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "sort.icon"),
+            style: .plain,
+            target: self,
+            action: nil
+        )
+        navigationItem.rightBarButtonItem?.tintColor = .black
     }
 }
