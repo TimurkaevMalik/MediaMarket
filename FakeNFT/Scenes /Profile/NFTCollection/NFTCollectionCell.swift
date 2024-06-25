25bd286ea3f578215833a669d5e66a0182ae7460
Malik <timurkaev_malik@icloud.com>
2024-06-25 10:34:25 +0300

    .

--- FakeNFT/Scenes /Profile/NFTCollection/NFTCollectionCell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NFTCollectionCell.swift
-//  FakeNFT
-//
-//  Created by Malik Timurkaev on 25.06.2024.
-//
-
-import Foundation
--- FakeNFT/Scenes /Profile/NFTCollection/RatingImageView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RatingImageView.swift
-//  FakeNFT
-//
-//  Created by Malik Timurkaev on 25.06.2024.
-//
-
-import Foundation
