26fa0ce38d4a3e0d491874b4c43d211d2c46f5ab
Malik <timurkaev_malik@icloud.com>
2024-06-25 10:32:07 +0300

    добавил titleLabel для закголовка экраа "Мои NFT"

--- FakeNFT/Scenes /Profile/NFTCollection/NFTCollectionController.swift
+++ FakeNFT/Scenes /Profile/NFTCollection/NFTCollectionController.swift
@@ -5,4 +5,45 @@
 //  Created by Malik Timurkaev on 25.06.2024.
 //
 
-import Foundation
+import UIKit
+
+
+final class NFTCollectionController: UIViewController {
+    
+    private lazy var titleLabel = UILabel()
+    private lazy var topViewsContainer = UIView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTopViewsContainer()
+        configureTitleLabel()
+    }
+    
+    private func configureTopViewsContainer() {
+        topViewsContainer.backgroundColor = .ypWhite
+        topViewsContainer.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(topViewsContainer)
+        
+        NSLayoutConstraint.activate([
+            topViewsContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            topViewsContainer.heightAnchor.constraint(equalToConstant: 42),
+            topViewsContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            topViewsContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        ])
+    }
+    
+    private func configureTitleLabel(){
+        titleLabel.text = "Мои NFT"
+        titleLabel.font = UIFont.bodyBold
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerYAnchor.constraint(equalTo: topViewsContainer.centerYAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: topViewsContainer.centerXAnchor)
+        ])
+    }
+}
